7fbc7eee023f7751c214c49964b2c0d6573b4341
SheepP <37098063+SheepP@users.noreply.github.com>
2019-03-05 14:52:49 +0800

    Add files via upload

---
+++ Dong/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Dong
+//
+//  Created by Sunny on 2019/3/5.
+//  Copyright © 2019 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollview: UIScrollView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
